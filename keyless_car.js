09d98d3e6f8a94fa2443f5ad3e5b33614450e301
Tyler Stevens <tylerr.stevens12@gmail.com>
2018-07-11 16:17:02 -0500

    Initial Javascript Function Submission

---
+++ keyless_car.js
@@ -0,0 +1,18 @@
+//Making a Keyless Car respond to a User
+
+//FUNCTION DECLARATION//
+
+//Function prompts user what their age is
+function checkDriverAge(){
+	prompt("What is your age?");
+}
+
+checkDriverAge();
+
+//FUNCTION EXPRESSION//
+var a = function checkDriverAge2(){
+	prompt("What is your age?");
+}
+
+checkDriverAge2();
+
