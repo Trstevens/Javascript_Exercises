4edb34af8afaf6b31e641e0549f481594a0b0faa
Tyler Stevens <tylerr.stevens12@gmail.com>
2018-07-11 16:34:12 -0500

    Added return portion of function

--- keyless_car.js
+++ keyless_car.js
@@ -28,6 +28,19 @@ var checkDriverAge2 = function(){
 }
 }
 
-
 checkDriverAge2();
 
+
+//BONUS Only use the return keyword
+
+function checkDriverAge3(age){
+	if (Number(age) < 18){
+		alert("Sorry you are too young to drive this car. Powering off.");
+	}else if (Number(age) > 18){
+		alert("Powering on. Enjoy the ride!");
+	}
+	else if (Number(age) == 18){
+		alert("Congratulations on your first year of driving. Enjoy the ride!");
+	}
+
+}
